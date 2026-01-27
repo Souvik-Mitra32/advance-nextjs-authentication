87beaf887c6ff9a6acd4e0ceba64c10b1a3e9d3c
Souvik-Mitra32 <souvikmitra1234@gmail.com>
2026-01-27 13:03:53 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function HomePage() {
+  return (
+    <div className="my-6 px-4 max-w-md mx-auto">
+      <div className="text-center space-y-6">
+        <h1 className="text-3xl font-bold">Welcome to Our App</h1>
+        <Link href="/auth/login">Sign up / Sign in</Link>
+      </div>
+    </div>
+  )
+}
